ce4b785f9aba2627ddf014ec455fb506d831fda9
hydrol0x <mrjacobry@gmail.com>
2023-12-17 14:23:25 -0500

    check for modifications and dont reprocess if not changed/new

--- static-site-generator.py
+++ static-site-generator.py
@@ -1,14 +1,23 @@
 import marko
 from pathlib import Path
 
-posts = Path("./posts")
-dist = Path("./dist")
+posts_dir = Path("./posts")
+dist_dir = Path("./dist")
 
-for post in posts.glob("*.md"):
-    html = ""
-    with open(post, 'r') as file:
-        html = marko.convert(file.read())
+# Create the dist directory if it does not exist
+dist_dir.mkdir(exist_ok=True)
 
-    write_name = dist / post.with_suffix('.html').name
-    with open(write_name, 'w') as file:
-        file.write(html)
+for post_path in posts_dir.glob("*.md"):
+    html_file_name = post_path.with_suffix('.html').name
+    html_file_path = dist_dir / html_file_name
+
+    # Check if the html file exists and compare modification times
+    if not html_file_path.exists() or post_path.stat().st_mtime > html_file_path.stat().st_mtime:
+        print(f"processing {post_path}")
+        # Read the Markdown file and convert it to HTML
+        with open(post_path, 'r', encoding='utf-8') as file:
+            html_content = marko.convert(file.read())
+
+        # Write the HTML content to the corresponding file
+        with open(html_file_path, 'w', encoding='utf-8') as file:
+            file.write(html_content)
